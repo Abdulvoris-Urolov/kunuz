817bcb306f6b85918ee6c5db51064dcaa95277da
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-10-08 21:41:07 +0500

    add file

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+//    useCreateIndex: true, 
+})
+.then(() => {
+    console.log('MongoDBga ulanish hosil qilindi...');
+})
+.catch((err) => {
+    console.error('MongoDBga ulanish vaqtida xato ro`y berdi...', err);
+});
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/", (req, res) =>{
+    console.log('main url');
+});
+//console.log('Abdulvoris');
+
+
+app.listen("2000", (req, res) =>{
+    console.log(`2000-port ishlayabdi`);
+});
---
+++ client/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/list", { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('MongoDBga ulanish hosil qilindi...');
+})
+.catch((err) => {
+    console.error('MongoDBga ulanish vaqtida xato ro`y berdi...', err);
+});
+
+app.listen(2000, (req, res) =>{
+    console.log(`2000-port ishlayabdi`);
+});
