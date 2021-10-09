9770816d159496e4b32f380054666d57a08fc653
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-10-09 20:47:29 +0500

    finished

--- api/index.js
+++ api/index.js
@@ -5,26 +5,35 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-//    useCreateIndex: true, 
-})
-.then(() => {
-    console.log('MongoDBga ulanish hosil qilindi...');
-})
-.catch((err) => {
-    console.error('MongoDBga ulanish vaqtida xato ro`y berdi...', err);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, })
+.then(console.log('MongoDBga ulanish hosil qilindi...'))
+.catch((err) => console.error('MongoDBga ulanish vaqtida xato ro`y berdi...', err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, "images")
+    },
+    filename:(req,file,cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded")
 });
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.use("/", (req, res) =>{
     console.log('main url');
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -42,9 +42,9 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        if(post.username === req.body.username){
+        if( post.username === req.body.username ){
             try {
-                await post
+                await post.delete();
                 res.status(200).json("Post has been deleted...");
             } catch (err) {
                 res.status(500).json(err);
@@ -61,12 +61,35 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/:id", async (req, res) => { 
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+//Get All Post
+
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if( username ){
+            posts = await Post.find({ username });
+        }else if(catName){
+            posts = await Post.find({
+            categories:{
+                $in:[catName],
+            },
+        });
+        }else{
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
